e20c6dd1cf1e4b712abfe8faf421c7b77c4c8d78
Nick Lay <layns@mail.uc.edu>
2022-11-26 15:24:38 -0500

    add some verbosity to the unit tests for clarity

--- src/Main.hs
+++ src/Main.hs
@@ -9,20 +9,25 @@ unit_test expected actual | expected == actual = "Pass"
 main :: IO ()
 main = do
   -- Test findFirst
+  putStrLn "findFirst Tests:"
   putStrLn $ unit_test (NoMatch) $ findFirst ((==) 1) []
   putStrLn $ unit_test (Match 0) $ findFirst ((/=) 1) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
   putStrLn $ unit_test (Match 9) $ findFirst ((==) 5) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
   putStrLn $ unit_test (Match 0) $ findFirst (11 >) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
   putStrLn $ unit_test (NoMatch) $ findFirst (const False) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
   putStrLn $ unit_test (NoMatch) $ findFirst ((==) 11) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+  putStrLn "\n"
   -- Test mergesort
+  putStrLn "mergesort Tests:"
   putStrLn $ unit_test [] $ mergesort ((>)::Integer->Integer->Bool) []
   putStrLn $ unit_test [1] $ mergesort (>) [1]
   putStrLn $ unit_test [1, 2] $ mergesort (<) [2,1]
   putStrLn $ unit_test [3, 2, 1] $ mergesort (>) [1, 2, 3]
   putStrLn $ unit_test [1,2,3,4,5,6,7,8,9,10] $ mergesort (<) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
   putStrLn $ unit_test [10,9,8,7,6,5,4,3,2,1] $ mergesort (>) [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+  putStrLn "\n"
   -- Test palindrome
+  putStrLn "palindrome Tests:"
   putStrLn $ unit_test True $ palindrome ""
   putStrLn $ unit_test True $ palindrome "a"
   putStrLn $ unit_test True $ palindrome "aa"
@@ -32,10 +37,13 @@ main = do
   putStrLn $ unit_test True $ palindrome "amanaplanacanalpanama"
   putStrLn $ unit_test True $ palindrome "madamimadam"
   putStrLn $ unit_test True $ palindrome "neveroddoreven"
+  putStrLn "\n"
   -- Test runLengthEncode
+  putStrLn "runLengthEncode Tests:"
   putStrLn $ unit_test [ ] $ runLengthEncode ""
   putStrLn $ unit_test [ (Span 1 'a') ] $ runLengthEncode "a"
   putStrLn $ unit_test [ (Span 1 'a'), (Span 1 'b') ] $ runLengthEncode "ab"
   putStrLn $ unit_test [ (Span 2 'a'), (Span 3 'd') ] $ runLengthEncode "aaddd"
   putStrLn $ unit_test [ (Span 2 '1'), (Span 2 '9') ] $ runLengthEncode "1199"
   putStrLn $ unit_test [ (Span 1 '1'), (Span 1 '2'), (Span 1 '3'), (Span 1 '4') ] $ runLengthEncode "1234"
+  putStrLn "\n"
