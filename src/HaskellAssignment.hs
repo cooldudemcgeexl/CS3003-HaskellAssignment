31500c90a78ff877fac8397ecd2699aabaab2730
Nick Lay <layns@mail.uc.edu>
2022-11-26 15:24:47 -0500

    finish palindrome

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,29 +1,36 @@
 module HaskellAssignment where
 
+import Text.ParserCombinators.ReadP (string)
+
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-data Found = Match Int | NoMatch deriving Eq
+data Found = Match Int | NoMatch deriving (Eq)
+
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
+
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 findFirst _ [] = NoMatch
-findFirst needleFunc (haystackH:haystackTail)
+findFirst needleFunc (haystackH : haystackTail)
   | needleFunc haystackH = Match 0
   | otherwise = findFirstWithIndex needleFunc haystackTail 1
 
 findFirstWithIndex :: (t -> Bool) -> [t] -> Int -> Found
 findFirstWithIndex _ [] _ = NoMatch
-findFirstWithIndex needleFunc (haystackH:haystackTail) currentIndex
-  | needleFunc haystackH = Match currentIndex 
-  | otherwise = findFirstWithIndex needleFunc haystackTail (currentIndex+1)
+findFirstWithIndex needleFunc (haystackH : haystackTail) currentIndex
+  | needleFunc haystackH = Match currentIndex
+  | otherwise = findFirstWithIndex needleFunc haystackTail (currentIndex + 1)
+
 ------------------------------------------------
 -- runLengthEncode
 ------------------------------------------------
-data RunLength = Span Integer Char deriving Eq
+data RunLength = Span Integer Char deriving (Eq)
+
 instance Show RunLength where
   show (Span length c) = "Length: " ++ show length ++ ": " ++ show c
+
 runLengthEncode :: [Char] -> [RunLength]
 runLengthEncode [] = []
 runLengthEncode string = []
@@ -31,9 +38,15 @@ runLengthEncode string = []
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
+-- Probably not the most efficient implementation since it checks the entire string.
+-- Could be optimized by only recursing to the halfway point.
+-- However, this gives a decently concise, tail-recursive definition.
 palindrome :: [Char] -> Bool
 palindrome [] = True
-palindrome string = True
+palindrome (stringHead : stringTail)
+  | stringHead /= last (stringHead : stringTail) = False
+  | otherwise = palindrome (take (length stringTail - 1) stringTail)
+
 ------------------------------------------------
 -- mergesort
 ------------------------------------------------
