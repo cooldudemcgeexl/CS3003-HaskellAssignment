ae9a3f9e3f38176a1e2aaa6dfabdab4434cd9be0
Nick Lay <layns@mail.uc.edu>
2022-11-26 18:18:10 -0500

    finish mergesort, cleanup

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -13,15 +13,19 @@ instance Show Found where
 
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 findFirst _ [] = NoMatch
-findFirst needleFunc (haystackH : haystackTail)
+findFirst needleFunc haystack
   | needleFunc haystackH = Match 0
-  | otherwise = findFirstWithIndex needleFunc haystackTail 1
+  | otherwise = findFirstWithIndex needleFunc haystackT 1
+  where
+    (haystackH : haystackT) = haystack
 
 findFirstWithIndex :: (t -> Bool) -> [t] -> Int -> Found
 findFirstWithIndex _ [] _ = NoMatch
-findFirstWithIndex needleFunc (haystackH : haystackTail) currentIndex
+findFirstWithIndex needleFunc haystack currentIndex
   | needleFunc haystackH = Match currentIndex
-  | otherwise = findFirstWithIndex needleFunc haystackTail (currentIndex + 1)
+  | otherwise = findFirstWithIndex needleFunc haystackT (currentIndex + 1)
+  where
+    (haystackH : haystackT) = haystack
 
 ------------------------------------------------
 -- runLengthEncode
@@ -30,7 +34,6 @@ data RunLength = Span Integer Char deriving (Eq)
 
 instance Show RunLength where
   show (Span length c) = "Length: " ++ show length ++ ": " ++ show c
-
 
 -- Group characters together, then map to the Span constructor
 runLengthEncode :: [Char] -> [RunLength]
@@ -45,13 +48,29 @@ runLengthEncode to_encode = map (\x -> Span (toInteger (length x)) (head x)) (gr
 -- However, this gives a decently concise, tail-recursive definition.
 palindrome :: [Char] -> Bool
 palindrome [] = True
-palindrome (candidateHead : candidateTail)
-  | candidateHead /= last (candidateHead : candidateTail) = False
-  | otherwise = palindrome (take (length candidateTail - 1) candidateTail)
+palindrome candidate
+  | candidateH /= last candidate = False
+  | otherwise = palindrome (take (length candidateT - 1) candidateT)
+  where
+    (candidateH : candidateT) = candidate
 
 ------------------------------------------------
 -- mergesort
 ------------------------------------------------
 mergesort :: (Ord a) => (a -> a -> Bool) -> [a] -> [a]
-mergesort ordFunc [] = []
-mergesort ordFunc list = list
+mergesort comparator [] = []
+mergesort comparator to_sort
+  | length to_sort == 1 = to_sort
+  | otherwise = merge comparator (mergesort comparator left) (mergesort comparator right)
+  where
+    (left, right) = splitAt (length to_sort `div` 2) to_sort
+
+merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ [] rightList = rightList
+merge _ leftList [] = leftList
+merge comparator leftList rightList
+  | comparator leftH rightH = leftH : merge comparator leftT rightList
+  | otherwise = rightH : merge comparator leftList rightT
+  where
+    (leftH : leftT) = leftList
+    (rightH : rightT) = rightList
