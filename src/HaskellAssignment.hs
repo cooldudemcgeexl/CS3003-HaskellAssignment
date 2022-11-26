f6323a740ef59534351f1122d486395a12286ee3
Nick Lay <layns@mail.uc.edu>
2022-11-25 22:00:24 -0500

    wip findFirst

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -8,7 +8,10 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-
+findFirst _ [] = NoMatch
+findFirst needleFunc (haystackH:haystackTail)
+  | needleFunc haystackH = Match 0
+  | otherwise = findFirst needleFunc haystackTail  
 ------------------------------------------------
 -- runLengthEncode
 ------------------------------------------------
@@ -16,13 +19,15 @@ data RunLength = Span Integer Char deriving Eq
 instance Show RunLength where
   show (Span length c) = "Length: " ++ show length ++ ": " ++ show c
 runLengthEncode :: [Char] -> [RunLength]
+runLengthEncode [] = []
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
-
+palindrome [] = True
 ------------------------------------------------
 -- mergesort
 ------------------------------------------------
 mergesort :: (Ord a) => (a -> a -> Bool) -> [a] -> [a]
+mergesort ordFunc [] = []
